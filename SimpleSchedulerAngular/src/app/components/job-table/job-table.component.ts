361ee7caa46b858a7c69baca9a6bd08c8fe2c5d3
Joseph Enos <ENOSJ@hs.azdhs.gov>
2021-04-16 09:54:32 -0700

    Getting middle click working, and properly showing dangerous data.

--- SimpleSchedulerAngular/src/app/components/job-table/job-table.component.ts
+++ SimpleSchedulerAngular/src/app/components/job-table/job-table.component.ts
@@ -4,6 +4,7 @@ import TimeSpan from 'src/app/models/timespan';
 import { JobService } from 'src/app/services/job.service';
 import { JobDetail } from "../../models/job-detail";
 import * as moment from "moment";
+import * as he from "he";
 
 @Component({
     selector: 'app-job-table',
@@ -30,14 +31,19 @@ export class JobTableComponent implements OnInit {
     }
 
     showDetail(e: MouseEvent, detailedMessage: string) {
+        e.preventDefault();
+        e.stopPropagation();
         switch (e.button) {
             case 0:
                 alert(detailedMessage);
                 break;
-            case 1: 
+            case 1:
                 const newWindow = window.open("about:blank", "_blank", "width=400,height=400,resizable");
-                newWindow?.document.write(detailedMessage);
-            break;
+                let val: string = he.encode(detailedMessage, { strict: true });
+                val = JSON.stringify(val);
+                newWindow?.document.write(`<div style="white-space:pre-line;"></div>`);
+                newWindow?.document.write(`<script>document.getElementsByTagName("div")[0].innerText = ${val}</script>`);
+                break;
         }
     }
 
