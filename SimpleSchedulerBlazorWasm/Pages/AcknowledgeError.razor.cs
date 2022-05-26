3737f91d6b8af0a517fccc40a19f2a66865a5de9
Joe Enos <jtenos1@gmail.com>
2022-05-26 14:18:33 -0700

    Acknowledging errors through email. Fixes #44

---
+++ SimpleSchedulerBlazorWasm/Pages/AcknowledgeError.razor.cs
@@ -0,0 +1,39 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using SimpleSchedulerApiModels.Reply.Jobs;
+using SimpleSchedulerApiModels.Request.Jobs;
+using SimpleSchedulerServiceClient;
+
+namespace SimpleSchedulerBlazorWasm.Pages;
+
+partial class AcknowledgeError
+{
+    [Inject]
+    private ServiceClient ServiceClient { get; set; } = default!;
+
+    [Inject]
+    private SweetAlertService Swal { get; set; } = default!;
+
+    [Parameter]
+    public Guid AcknowledgementCode { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        (Error? error, AcknowledgeErrorReply? reply) = await ServiceClient.PostAsync<AcknowledgeErrorRequest, AcknowledgeErrorReply>(
+            "Jobs/AcknowledgeError",
+            new AcknowledgeErrorRequest(AcknowledgementCode: AcknowledgementCode)
+        );
+
+        if (error is not null)
+        {
+            await Swal.FireAsync("Error", error.Message, SweetAlertIcon.Error);
+            return;
+        }
+
+        await Swal.FireAsync(
+            title: "Error Acknowledged",
+            message: "The error has been successfully acknowledged",
+            icon: SweetAlertIcon.Success
+        );
+    }
+}
