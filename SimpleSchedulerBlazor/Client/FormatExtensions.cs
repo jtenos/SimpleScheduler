181589c8e0c93b6db1b1c0ed5c6d9f5929a8c655
Joe Enos <jtenos1@gmail.com>
2022-05-26 15:07:22 -0700

    Removing old Blazor projects

--- SimpleSchedulerBlazor/Client/FormatExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleSchedulerBlazor.Client;
-
-public static class FormatExtensions
-{
-    public static string FormatDate(this DateTime? dt) => dt.HasValue ? FormatDate(dt.Value) : "";
-    public static string FormatDate(this DateTime dt) => dt.ToString("MMM dd yyyy");
-    public static string FormatTime(this DateTime? dt) => dt.HasValue ? FormatTime(dt.Value) : "";
-    public static string FormatTime(this DateTime dt) => $"{dt:HH\\:mm\\:ss} (UTC)";
-}
