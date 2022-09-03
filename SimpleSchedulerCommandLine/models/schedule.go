1040c7a051989821c0e588a829d66f7915b7d9a6
Joe Enos <jtenos1@gmail.com>
2022-09-02 23:17:18 -0700

    Showing worker details.

--- SimpleSchedulerCommandLine/models/schedule.go
+++ SimpleSchedulerCommandLine/models/schedule.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Schedule struct {
 	ID                   int64  `json:"id"`
 	IsActive             bool   `json:"isActive"`
@@ -16,4 +22,77 @@ type Schedule struct {
 	RecurBetweenStartUTC string `json:"recurBetweenStartUTC"`
 	RecurBetweenEndUTC   string `json:"recurBetweenEndUTC"`
 	OneTime              bool   `json:"oneTime"`
+}
+
+func tern(b bool, first string, second string) string {
+	if b {
+		return first
+	}
+	return second
+}
+
+func getFormattedTime(ts string) string {
+	if len(ts) == 0 {
+		return ""
+	}
+
+	fields := strings.Split(ts, ":")
+	hours, _ := strconv.Atoi(fields[0])
+	minutes, _ := strconv.Atoi(fields[1])
+
+	if hours == 1 && minutes == 0 {
+		return "every hour"
+	}
+	if hours > 1 && minutes == 0 {
+		return fmt.Sprintf("every %v hours", hours)
+	}
+	if hours > 0 && minutes > 0 {
+		return fmt.Sprintf("every %s", ts)
+	}
+	if minutes == 1 {
+		return "every minute"
+	}
+	if minutes > 1 {
+		return fmt.Sprintf("every %v minutes", minutes)
+	}
+	return "unknown"
+}
+
+func (sch *Schedule) GetFormatted() string {
+	var days string
+	if sch.Sunday && sch.Monday && sch.Tuesday && sch.Wednesday && sch.Thursday && sch.Friday && sch.Saturday {
+		days = "Every day"
+	} else if !sch.Sunday && sch.Monday && sch.Tuesday && sch.Wednesday && sch.Thursday && sch.Friday && !sch.Saturday {
+		days = "Weekdays"
+	} else if sch.Sunday && !sch.Monday && !sch.Tuesday && !sch.Wednesday && !sch.Thursday && !sch.Friday && sch.Saturday {
+		days = "Weekends"
+	} else {
+		days = fmt.Sprintf("%s %s %s %s %s %s %s",
+			tern(sch.Sunday, "Su", "__"),
+			tern(sch.Monday, "Mo", "__"),
+			tern(sch.Tuesday, "Tu", "__"),
+			tern(sch.Wednesday, "We", "__"),
+			tern(sch.Thursday, "Th", "__"),
+			tern(sch.Friday, "Fr", "__"),
+			tern(sch.Saturday, "Sa", "__"),
+		)
+	}
+
+	times := "Unknown"
+
+	if len(sch.TimeOfDayUTC) > 0 {
+		times = fmt.Sprintf("at %s", sch.TimeOfDayUTC)
+	} else if len(sch.RecurTime) > 0 {
+		times = getFormattedTime(sch.RecurTime)
+	}
+
+	if len(sch.RecurBetweenStartUTC) > 0 && len(sch.RecurBetweenEndUTC) > 0 {
+		times += fmt.Sprintf(" between %s and %s", sch.RecurBetweenStartUTC, sch.RecurBetweenEndUTC)
+	} else if len(sch.RecurBetweenStartUTC) > 0 {
+		times += fmt.Sprintf(" starting at %s", sch.RecurBetweenStartUTC)
+	} else if len(sch.RecurBetweenEndUTC) > 0 {
+		times += fmt.Sprintf(" until %s", sch.RecurBetweenEndUTC)
+	}
+
+	return fmt.Sprintf("%s [%s]", days, times)
 }
--- SimpleSchedulerCommandLine/ui/workers.go
+++ SimpleSchedulerCommandLine/ui/workers.go
@@ -28,23 +28,83 @@ func listWorkers() {
 		return
 	}
 
+	// Sort by worker name
 	sort.Slice(rep.Workers, func(i, j int) bool {
-		return rep.Workers[i].Worker.WorkerName < rep.Workers[j].Worker.WorkerName
+		w1 := rep.Workers[i].Worker
+		w2 := rep.Workers[j].Worker
+
+		var compare int
+		if w1.IsActive && w2.IsActive {
+			compare = 0
+		} else if w1.IsActive {
+			compare = -1
+		} else if w2.IsActive {
+			compare = 1
+		}
+		switch compare {
+		case 0:
+			return w1.WorkerName < w2.WorkerName
+		case 1:
+			return false
+		case -1:
+			return true
+		}
+		return false
 	})
+
 	menuItems := make([]*menuItem, len(rep.Workers))
 	for i := range rep.Workers {
+		wws := &rep.Workers[i]
 		worker := &rep.Workers[i].Worker
-		menuItems[i] = newMenuItem(rep.Workers[i].Worker.WorkerName, func() { showWorker(worker) })
+		title := worker.WorkerName
+		if !worker.IsActive {
+			title += " (INACTIVE)"
+		}
+		menuItems[i] = newMenuItem(title, func() { showWorker(wws, rep.Workers) })
 	}
+
 	newMenu("WORKERS",
 		menuItems,
 		showWorkers,
 	).show()
 }
 
-func showWorker(worker *models.Worker) {
-	fmt.Printf("Worker: %s\n", worker.WorkerName)
-	// TODO: Details
+func getWorkerName(workerID int64, allWorkers []models.WorkerWithSchedules) string {
+	for i := range allWorkers {
+		if allWorkers[i].Worker.ID == workerID {
+			return allWorkers[i].Worker.WorkerName
+		}
+	}
+	return "NOT FOUND"
+}
+
+func showWorker(worker *models.WorkerWithSchedules, allWorkers []models.WorkerWithSchedules) {
+	fmt.Println("")
+	fmt.Printf("Worker: %s", worker.Worker.WorkerName)
+	if worker.Worker.IsActive {
+		fmt.Printf(" (active)\n")
+	} else {
+		fmt.Printf(" (INACTIVE)\n")
+	}
+	if len(worker.Worker.DetailedDescription) > 0 {
+		fmt.Printf("  Description: %s\n", worker.Worker.DetailedDescription)
+	}
+	if len(worker.Worker.EmailOnSuccess) > 0 {
+		fmt.Printf("  Email on Success: %s\n", worker.Worker.EmailOnSuccess)
+	}
+	if worker.Worker.ParentWorkerID > 0 {
+		fmt.Printf("  Parent worker: %s\n", getWorkerName(worker.Worker.ParentWorkerID, allWorkers))
+	}
+	fmt.Printf("  Timeout minutes: %v\n", worker.Worker.TimeoutMinutes)
+	fmt.Printf("  Directory: %s\n", worker.Worker.DirectoryName)
+	fmt.Printf("  Executable: %s\n", worker.Worker.Executable)
+	if len(worker.Worker.ArgumentValues) > 0 {
+		fmt.Printf("Arguments: %s\n", worker.Worker.ArgumentValues)
+	}
+	fmt.Println("  Schedules:")
+	for _, sch := range worker.Schedules {
+		fmt.Printf("    %s\n", sch.GetFormatted())
+	}
 }
 
 func searchWorkers() {
