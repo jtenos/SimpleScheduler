1f62cfc0976ccbb91b240108ce33cd98d8578415
Joe Enos <jtenos1@gmail.com>
2022-10-08 15:59:53 -0700

    Adding Blazorise, updating references.

--- SimpleSchedulerBlazorWasm/Program.cs
+++ SimpleSchedulerBlazorWasm/Program.cs
@@ -1,4 +1,7 @@
 using Blazored.LocalStorage;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -12,30 +15,32 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
 
-builder.Services.AddSweetAlert2(options =>
-{
-    options.Theme = SweetAlertTheme.Bootstrap4;
-});
+builder.Services.AddSweetAlert2(options => options.Theme = SweetAlertTheme.Bootstrap4);
+
+builder.Services
+	.AddBlazorise(options => options.Immediate = true)
+	.AddBootstrap5Providers()
+	.AddFontAwesomeIcons();
 
 builder.Services.AddScoped<ITokenLookup, TokenLookup>();
 
 builder.Services.AddScoped(sp =>
 {
-    ITokenLookup tokenLookup = sp.GetRequiredService<ITokenLookup>();
+	ITokenLookup tokenLookup = sp.GetRequiredService<ITokenLookup>();
 
-    Console.WriteLine("Creating new ServiceClient");
-    NavigationManager navManager = sp.GetRequiredService<NavigationManager>();
-    void redirectToLogin() => navManager.NavigateTo("login");
-    ServiceClient sc = new(
-        httpClient: new HttpClient { BaseAddress = new(builder.Configuration["ApiUrl"]!) },
-        tokenLookup: tokenLookup,
-        redirectToLogin: redirectToLogin,
-        logger: sp.GetRequiredService<ILogger<ServiceClient>>()
-    );
+	Console.WriteLine("Creating new ServiceClient");
+	NavigationManager navManager = sp.GetRequiredService<NavigationManager>();
+	void redirectToLogin() => navManager.NavigateTo("login");
+	ServiceClient sc = new(
+		httpClient: new HttpClient { BaseAddress = new(builder.Configuration["ApiUrl"]!) },
+		tokenLookup: tokenLookup,
+		redirectToLogin: redirectToLogin,
+		logger: sp.GetRequiredService<ILogger<ServiceClient>>()
+	);
 
-    Console.WriteLine("ServiceClient is created");
+	Console.WriteLine("ServiceClient is created");
 
-    return sc;
+	return sc;
 });
 
 builder.Services.AddSingleton<ClientAppInfo>();
